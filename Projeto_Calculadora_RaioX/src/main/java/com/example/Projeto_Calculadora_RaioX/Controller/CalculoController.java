661dbcbf2c98fc32a4f1cb43c4ac53715bb92fd0
matheus.rossato <matheusrossato2002@hotmail.com>
2024-06-28 15:50:49 -0300

    Pronto

--- Projeto_Calculadora_RaioX/src/main/java/com/example/Projeto_Calculadora_RaioX/Controller/CalculoController.java
+++ Projeto_Calculadora_RaioX/src/main/java/com/example/Projeto_Calculadora_RaioX/Controller/CalculoController.java
@@ -1,13 +1,20 @@
 package com.example.Projeto_Calculadora_RaioX.Controller;
 
 import com.example.Projeto_Calculadora_RaioX.Model.Calculo;
+import com.example.Projeto_Calculadora_RaioX.Repository.CalculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/calculos")
 public class CalculoController {
+
+    @Autowired
+    private CalculoRepository calculoRepository;
 
     @GetMapping("/calcular")
     public String showForm(Model model) {
@@ -18,7 +25,13 @@ public class CalculoController {
     @PostMapping("/calcular")
     public String calcular(@ModelAttribute Calculo calculo, Model model) {
         calculo.Calcular();
+
+        calculoRepository.save(calculo);
+
+        List<Calculo> calculos = calculoRepository.findAll();
+
         model.addAttribute("calculo", calculo);
+        model.addAttribute("calculos", calculos);
 
         return "resultados";
     }
